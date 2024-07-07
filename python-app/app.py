a9b7d9da9f720b6c3aa6d193fc6058ef178e588d
MuneendraSingam <muneendra7777s@gmail.com>
2024-07-07 16:18:14 -0400

    Change in app.py

--- python-app/app.py
+++ python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, I am Muneendra Singam'
+    return 'Hello, I am Muneendra Reddy Singam'
 
 if __name__ == '__main__':
     app.run()
