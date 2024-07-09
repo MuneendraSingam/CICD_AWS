3ece7122e79b941f0442fd24c09cd5528d34e0c4
MuneendraSingam <161382554+MuneendraSingam@users.noreply.github.com>
2024-07-09 14:13:43 -0400

    edited container start

--- scripts/containerstart.sh
+++ scripts/containerstart.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull muneendrasingam/python-app:latest
+docker pull muneendrasingam/python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 muneendrasingam/python-app:latest
+docker run -d -p 5000:5000 muneendrasingam/python-app
