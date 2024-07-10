4fe06788e2d3800c93ec744cc6b56c17317fbbd3
MuneendraSingam <161382554+MuneendraSingam@users.noreply.github.com>
2024-07-09 20:51:59 -0400

    edit

--- python-app/app.py
+++ python-app/app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, I am Muneendra Reddy Singa. I am a junior DevOps Engineer'
+    return 'Hello, I am Muneendra Reddy Singam. I am a junior DevOps Engineer'
 
 if __name__ == '__main__':
     app.run()
-
-    
