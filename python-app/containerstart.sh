fe7eb6a6a42603c58723a0d96af5b1bd27d9033f
MuneendraSingam <161382554+MuneendraSingam@users.noreply.github.com>
2024-07-12 08:47:05 -0400

    edit

--- python-app/containerstart.sh
+++ python-app/containerstart.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull muneendrasingam/python-app
+docker pull muneendrasingam/python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 6000:6000 muneendrasingam/python-app
+docker run -d -p 6000:6000 muneendrasingam/python-flask-app
--- scripts/containerstop.sh
+++ scripts/containerstop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-containerID='docker ps|awk -F " " '{print $1}''
-docker rm -f $containerID
+echo "Hi"
